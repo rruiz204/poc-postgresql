f9754ea788c62c0a6179f3555aa784440daa6961
roney ruiz <roneyruizrojas@gmail.com>
2025-04-28 15:19:24 -0600

    feat: create fake data to insert

---
+++ src/domain/user/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO library.user (first_name, last_name, email, phone) VALUES
+('María', 'González', 'maria.gonzalez@example.com', '+506 1234 5678'),
+('Juan', 'Pérez', 'juan.perez@example.com', '+506 2345 6789'),
+('Ana', 'Rodríguez', 'ana.rodriguez@example.com', '+506 3456 7890'),
+('Carlos', 'Martínez', 'carlos.martinez@example.com', '+506 4567 8901'),
+('Laura', 'Sánchez', 'laura.sanchez@example.com', '+506 5678 9012');
